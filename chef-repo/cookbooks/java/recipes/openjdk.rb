d2752fff4a4adb57bcfbf69455e48bbff4490fd4
root <root@ip-172-31-19-181.ap-south-1.compute.internal>
2018-03-08 11:13:45 +0000

    apache complteted cookbook

--- chef-repo/cookbooks/java/recipes/openjdk.rb
+++ chef-repo/cookbooks/java/recipes/openjdk.rb
@@ -1,11 +1,9 @@
 # Author:: Bryan W. Berry (<bryan.berry@gmail.com>)
-# Author:: Seth Chisamore (<schisamo@chef.io>)
-# Author:: Joshua Timberman (<joshua@chef.io>)
-#
-# Cookbook:: java
+# Author:: Seth Chisamore (<schisamo@opscode.com>)
+# Cookbook Name:: java
 # Recipe:: openjdk
 #
-# Copyright:: 2010-2015, Chef Software, Inc.
+# Copyright 2010-2011, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,63 +17,67 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'java::notify'
+version = node['java']['jdk_version']
+java_home = node['java']['java_home']
+java_home_parent = File.dirname java_home
+jdk_home = ""
 
-unless node.recipe?('java::default')
-  Chef::Log.warn('Using java::default instead is recommended.')
+pkgs = value_for_platform(
+  ["centos","redhat","fedora"] => {
+    "default" => ["java-1.#{version}.0-openjdk","java-1.#{version}.0-openjdk-devel"]
+  },
+  ["arch","freebsd"] => {
+    "default" => ["openjdk#{version}"]
+  },
+  "default" => ["openjdk-#{version}-jdk"]
+  )
 
-  # Even if this recipe is included by itself, a safety check is nice...
-  [node['java']['openjdk_packages'], node['java']['java_home']].each do |v|
-    include_recipe 'java::set_attributes_from_version' if v.nil? || v.empty?
+# done by special request for rberger
+ruby_block  "set-env-java-home" do
+  block do
+    ENV["JAVA_HOME"] = java_home
   end
 end
 
-jdk = Opscode::OpenJDK.new(node)
+if platform?("ubuntu","debian","redhat","centos","fedora","scientific","amazon")
+  ruby_block "update-java-alternatives" do
+    block do
+      if platform?("ubuntu", "debian") and version == 8
+        run_context = Chef::RunContext.new(node, {})
+        r = Chef::Resource::Execute.new("update-java-alternatives", run_context)
+        r.command "update-java-alternatives -s java-8-openjdk"
+        r.returns [0,2]
+        r.run_action(:create)
+      else
+        # have to do this on ubuntu for version 7 because Ubuntu does
+        # not currently set jdk 7 as the default jvm on installation
+        require "fileutils"
+        arch = node['kernel']['machine'] =~ /x86_64/ ? "x86_64" : "i386"
+        Chef::Log.debug("glob is #{java_home_parent}/java*#{version}*openjdk*")
+        jdk_home = Dir.glob("#{java_home_parent}/java*#{version}*openjdk{,[-\.]#{arch}}")[0]
+        Chef::Log.debug("jdk_home is #{jdk_home}")
+        # delete the symlink if it already exists
+        if File.exists? java_home
+          FileUtils.rm_f java_home
+        end
+        FileUtils.ln_sf jdk_home, java_home
 
-if platform_requires_license_acceptance?
-  file '/opt/local/.dlj_license_accepted' do
-    owner 'root'
-    group 'root'
-    mode '0400'
-    action :create
-    only_if { node['java']['accept_license_agreement'] }
+        cmd = Chef::ShellOut.new(
+          %Q[ update-alternatives --install /usr/bin/java java #{java_home}/bin/java 1;
+             update-alternatives --set java #{java_home}/bin/java  ]
+          ).run_command
+        unless cmd.exitstatus == 0 or  cmd.exitstatus == 2
+          Chef::Application.fatal!("Failed to update-alternatives for openjdk!")
+        end
+      end
+    end
+    action :nothing
   end
 end
 
-if node['platform'] == 'ubuntu'
-  apt_repository 'openjdk-r-ppa' do
-    uri 'ppa:openjdk-r'
-    distribution node['lsb']['codename']
-  end
-end
-
-node['java']['openjdk_packages'].each do |pkg|
+pkgs.each do |pkg|
   package pkg do
-    version node['java']['openjdk_version'] if node['java']['openjdk_version']
-    notifies :write, 'log[jdk-version-changed]', :immediately
+    action :install
+    notifies :create, "ruby_block[update-java-alternatives]"  if platform?("ubuntu","debian","redhat","centos","fedora","scientific","amazon")
   end
 end
-
-java_alternatives 'set-java-alternatives' do
-  java_location jdk.java_home
-  default node['java']['set_default']
-  priority jdk.alternatives_priority
-  case node['java']['jdk_version'].to_s
-  when '6'
-    bin_cmds node['java']['jdk']['6']['bin_cmds']
-  when '7'
-    bin_cmds node['java']['jdk']['7']['bin_cmds']
-  when '8'
-    bin_cmds node['java']['jdk']['8']['bin_cmds']
-  end
-  action :set
-  only_if { platform_family?('debian', 'rhel', 'fedora') }
-end
-
-if node['java']['set_default'] && platform_family?('debian')
-  include_recipe 'java::default_java_symlink'
-end
-
-# We must include this recipe AFTER updating the alternatives or else JAVA_HOME
-# will not point to the correct java.
-include_recipe 'java::set_java_home'
