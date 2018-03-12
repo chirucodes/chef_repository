4fe4ee4e48c5bb90b51ae3cb50d121ccebb53f88
duddumounika <duddumounika@gmail.com>
2018-03-12 19:26:06 +0530

    Update solo.rb

--- chef/chef-repo/solo.rb
+++ chef/chef-repo/solo.rb
@@ -1,3 +1,3 @@
-file_cache_path "/root/sample123/chef-solo"
-cookbook_path "/root/sample123/chef-repo/cookbooks"
+file_cache_path "/root/chef_repository/chef/chef-solo"
+cookbook_path "/root/chef_repository/chef/chef-repo/cookbooks"
 
